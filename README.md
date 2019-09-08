03bbe31667532b22bfbab7b5004988c6dc5b0c4c
iceyang <w@icey.cc>
2019-09-08 13:12:48 +0800

    数据类型和变量

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-https://www.zhihu.com/question/30315894/answer/154979413
-https://www.jianshu.com/p/b7cc0e36cd5f
+
+
+# c语言相关
+## 1 问题整理
+1. 打开exe文件闪退解决方案 https://blog.csdn.net/hitwhylz/article/details/14120515
+2. https://www.zhihu.com/question/30315894/answer/154979413  
+3. https://www.jianshu.com/p/b7cc0e36cd5f
--- src/hollow.c
+++ src/hollow.c
@@ -3,26 +3,43 @@
 int main()
 {
 
-    //  C语言变量和数据类型
-    // 变量 variable
+    // 一 、C语言变量和数据类型
+    // 1、变量 variable
     // = 赋值 assign 赋值是指把数据放到内存的过程
-    // 数据类型
-    // char 字符型
-    // int      整数 integer
-    // short    短整型
-    // long     长整型
-    // float    单精度浮点型
-    
-    int a1 = 20, a2 = 345, a3 = 700, a4 = 22;
-    int b1 = 56720, b2 = 9999, b3 = 20098, b4 = 2;
-    int c1 = 233, c2 = 205, c3 = 1, c4 = 6666;
-    int d1 = 34, d2 = 0, d3 = 23, d4 = 23006783;
-    // printf() 的高级用法
-    // %[flag][width][.precision]type
-    printf("%-9d %-9d %-9d %-9d\n", a1, a2, a3, a4);
-    printf("%-9d %-9d %-9d %-9d\n", b1, b2, b3, b4);
-    printf("%-9d %-9d %-9d %-9d\n", c1, c2, c3, c4);
-    printf("%-9d %-9d %-9d %-9d\n", d1, d2, d3, d4);
-    printf("hello world11!/n");
+    // 2、数据类型
+    // 数据类型除了指明数据的解释方式，还指明了数据的长度
+    // char 字符型  1
+    // short    短整型 2
+    // int（integer） 整数   4
+    // long     长整型 4
+    // float    单精度浮点型 4
+    // double   双精度浮点型 8
+    // void     无类型
+    // 3、总结
+    // - 数据是存在内存中的，在内存中存取数据要明确三件事情：
+    // ==> 数据存储在哪里?
+    // ==> 数据的长度？
+    // ==> 数据的处理方式？
+    // 变量名不仅给变量起了个好记得名字，还告诉我们数据存储在哪里
+    // 数据类型告诉我们数据的长度和处理方式
+    // -  强数据类型 + 弱数据类型
+
+    // 二、在屏幕上输出各种类型的数据
+    // 1、puts("output string"); // <output string> 只能用来输出字符串 。puts 输出完成后会自动换行
+    // 2、printf("printf"); // 可以输出整数、小数、单个字符串，输出格式可以自定义。 printf 不会自动换行，要自己添加换行符 \n
+    // %d: 输出一个十进制整形。d 是 decimal 的简写。表示以十进制整数的形式输出
+    // %c：输出一个字符。c 是 character 的简写。
+    // %s：输出一个字符串。s 是 string 的简写。
+    // %f：输出一个小数。f 是 float 的简写。
+    // 注意字符串必须是双引号
+    printf("printf");
+    int abc = 999;
+    printf("The value of abc is %d !", abc); // decimal 十进制。abc 替换前面的 %d
+    int n = 100;
+    char c = '@';  //字符用单引号包围，字符串用双引号包围
+    float money = 93.96;
+    printf("n=%d, c=%c, money=%f\n", n, c, money); // 前后一一对应
+
+    getch(); // 
     system("pause");
 }
